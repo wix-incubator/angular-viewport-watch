d18dfd127c4138da20268ea1105201915f9d60c5
builduser <builduser@wix.com>
2017-04-01 02:07:08 +0000

    release 0.1.34

--- dist/bower_components/angular-mocks/angular-mocks.js
+++ dist/bower_components/angular-mocks/angular-mocks.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.6.3
+ * @license AngularJS v1.6.4
  * (c) 2010-2017 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -797,7 +797,7 @@ angular.mock.TzDate.prototype = Date.prototype;
  * You need to require the `ngAnimateMock` module in your test suite for instance `beforeEach(module('ngAnimateMock'))`
  */
 angular.mock.animate = angular.module('ngAnimateMock', ['ng'])
-  .info({ angularVersion: '1.6.3' })
+  .info({ angularVersion: '1.6.4' })
 
   .config(['$provide', function($provide) {
 
@@ -1140,6 +1140,8 @@ angular.mock.dump = function(object) {
     $http.get('/auth.py').then(function(response) {
       authToken = response.headers('A-Token');
       $scope.user = response.data;
+    }).catch(function() {
+      $scope.status = 'Failed...';
     });
 
     $scope.saveMessage = function(message) {
@@ -2414,7 +2416,7 @@ angular.module('ngMock', ['ng']).provider({
   $provide.decorator('$rootScope', angular.mock.$RootScopeDecorator);
   $provide.decorator('$controller', createControllerDecorator($compileProvider));
   $provide.decorator('$httpBackend', angular.mock.$httpBackendDecorator);
-}]).info({ angularVersion: '1.6.3' });
+}]).info({ angularVersion: '1.6.4' });
 
 /**
  * @ngdoc module
@@ -2429,7 +2431,7 @@ angular.module('ngMock', ['ng']).provider({
  */
 angular.module('ngMockE2E', ['ng']).config(['$provide', function($provide) {
   $provide.decorator('$httpBackend', angular.mock.e2e.$httpBackendDecorator);
-}]).info({ angularVersion: '1.6.3' });
+}]).info({ angularVersion: '1.6.4' });
 
 /**
  * @ngdoc service
