e53a5486770c5c4b2817e710dbbecf049ee04be9
builduser <builduser@wix.com>
2017-04-27 08:55:47 +0000

    release 0.1.35

--- dist/views/main.html.js
+++ dist/views/main.html.js
@@ -11,16 +11,16 @@ angular.module('angularViewportWatchAppInternal').run(['$templateCache', functio
 
   $templateCache.put('views/main.html',
     "<div class='hero-unit' ng-controller='MainController as main'>\n" +
-    "  <h1>\n" +
-    "    {{'general.YO' | translate}}\n" +
-    "    <i class='logo-on-header angular-viewport-watch-svg-font-icons-wix-logo'></i>\n" +
-    "  </h1>\n" +
-    "  <p>You now have</p>\n" +
-    "  <ul>\n" +
-    "    <li ng-repeat='thing in main.awesomeThings'>{{thing}}</li>\n" +
-    "  </ul>\n" +
-    "  <p>installed.</p>\n" +
-    "  <h3>Enjoy coding! - Yeoman</h3>\n" +
+    "<h1>\n" +
+    "{{'general.YO' | translate}}\n" +
+    "<i class='logo-on-header angular-viewport-watch-svg-font-icons-wix-logo'></i>\n" +
+    "</h1>\n" +
+    "<p>You now have</p>\n" +
+    "<ul>\n" +
+    "<li ng-repeat='thing in main.awesomeThings'>{{thing}}</li>\n" +
+    "</ul>\n" +
+    "<p>installed.</p>\n" +
+    "<h3>Enjoy coding! - Yeoman</h3>\n" +
     "</div>\n"
   );
 }]);
